bf4a3211c9ea6f41b492fae8cf2fb20f7b7ecc21
Estiven Valencia <Estivenvalencia0863@gmaiil.com>
2025-04-20 19:33:05 -0500

    continuacion

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -5,7 +5,10 @@ const Inicio = () => {
   return (
     <div className='aplicativo'>
       <MenuSuperior />
+      <h1 className='letra-inicio'> Bienvenidos a Nuestro Mundo </h1>
+      <h1 className='letra-inicio-pequeña'> Donde cada Pagina cuenta una Historia </h1>
     </div>
+    
    
     
   )
